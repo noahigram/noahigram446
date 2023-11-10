4a41fa94b49bdef7ee682d9b670c616378f384f9
noahigram <75544386+noahigram@users.noreply.github.com>
2023-11-10 08:58:42 -0600

    Update finite.py

--- finite.py
+++ finite.py
@@ -41,6 +41,16 @@ class NonUniformPeriodicGrid:
 
         return dx
 
+class UniformNonPeriodicGrid:
+
+    def __init__(self, N, interval):
+        """ Non-uniform grid; no grid points at the endpoints of the interval"""
+        self.start = interval[0]
+        self.end = interval[1]
+        self.dx = (self.end - self.start)/(N-1)
+        self.N = N
+        self.values = np.linspace(self.start, self.end, N, endpoint=True)
+
 
 class Domain:
 
